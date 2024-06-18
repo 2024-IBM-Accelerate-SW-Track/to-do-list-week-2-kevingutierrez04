983ba36f280ed69ed24de8603dc944bebfdff9fc
kevingutierrez04 <kevingut04@hotmail.com>
2024-06-17 23:36:10 -0400

    let's hope this passes everything

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,31 +1,37 @@
 import React, { Component } from "react";
 import "./About.css";
-import profile_pic from "../assets/profile_pic.jpg";
+import profile_pic from "../assets/profile_pic.png";
 
 export default class About extends Component {
   render() {
     return (
       <div>
-        {/* <p>Design your About me page </p> */}
-        <div class="split left">
-          <div className="centered">
-            <img
-              className="profile_image"
-              src={profile_pic}
-              alt="Profile Pic"
-            ></img>
-          </div>
-        </div>
-        <div className="split right">
-          <div className="centered">
-            <div className="name_title">Your Name</div>
-            <div className="brief_description">
-              Tell us about yourself in a few sentences. Tell us your interests
-              and say a fun fact about yourself.
-            </div>
-          </div>
-        </div>
+      <div>
+      <div className="split left">
+      <div className="centered">
+      <img 
+      className="profile_image"
+      src = {profile_pic}
+      alt="Profile Pic"
+      ></img>
       </div>
-    );
+      </div>
+      <div className="split right">
+      <div className="centered">
+      <div className="name_title">Kevin Gutierrez Garcia</div>
+      <div className="brief_description">
+      <p>CS + Econ at Columbia<br/>
+      Rising Junior<br/>
+      Love Running, Skiing, and playing Chess<br/>
+      Involvements on Campus:<br/>
+      Columbia Spectator Engineering Team <br/>
+      Columbia Organization of Rising Entrepeneurs <br/>
+      Society of Hispanic Professional Engineers</p>
+      </div>
+      </div>
+      </div>
+      </div>
+      </div>
+    )
   }
 }
